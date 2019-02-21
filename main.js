60be1a8ad30a47e3683c24d8fccccea7c5cf0337
Mintae Lee <lee.mintae@codeimmersives.com>
2019-02-21 12:43:43 -0500

    added js commands for title case and my choice

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
 window.onload = init;
 
 function init() {
-    document.querySelector('#input')
-        .addEventListener('keydown', handleTyping);
+    document.querySelector('.input')
+        .addEventListener('keyup', handleTyping);
+    document.querySelector('.input2')
+        .addEventListener('keyup', handleSecondTyping);
 }
 
 function handleTyping(event) {
-    let userInput = event.target.value;
-    let answer = userInput;
-    
-    document.querySelector('#changeme').innerText = answer;
+    let userInput = event.target.value.toLowerCase();
+    let answer = '';
+    i = 0;
+    while (i < userInput.length) {
+        if (i === 0  || userInput[i-1] === ' ') {
+            answer += userInput.charAt(i).toUpperCase();
+        } else {
+            answer += userInput[i];
+        }
+        i++;
+    };
+
+    document.querySelector('.changeme').innerText = answer;
+}
+
+function handleSecondTyping(event) {
+    let userInput = event.target.value.toLowerCase();
+    let answer = '';
+    for (i = 0; i < userInput.length; i++){
+        if (i % 2 === 0 && userInput[i] !== ' '){
+            answer += userInput.charAt(i).toUpperCase();
+        } else {
+            answer += userInput[i];
+        }
+    }
+
+    document.querySelector('.changeme2').innerText = answer;
 }
